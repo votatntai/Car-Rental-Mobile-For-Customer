afe16f2b749287a966858d7f50f52d666308c8e5
Trung Hieu <trunghieunguyendn@gmail.com>
2023-03-12 21:28:07 +0700

    add env

--- lib/commons/constants/maps.dart
+++ lib/commons/constants/maps.dart
@@ -1,1 +1,3 @@
-String mapsApiKey = '';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+String mapsApiKey = dotenv.env['GOOGLE_MAPS_API_KEY'] ?? '';
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:car_rental_for_customer/app/app.dart';
 import 'package:car_rental_for_customer/bootstrap.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 Future<void> main() async {
+  await dotenv.load(fileName: '.env');
   await bootstrap(() => const App());
 }
