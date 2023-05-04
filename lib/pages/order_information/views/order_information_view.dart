10987b5d7991f06868c508a791ce140c4d0bceea
Trung Hieu <hieunt0220@gmail.com>
2023-05-05 00:00:57 +0700

    minro fix

--- lib/pages/order_information/views/order_information_view.dart
+++ lib/pages/order_information/views/order_information_view.dart
@@ -600,9 +600,9 @@ class _OrderInformationViewState extends State<OrderInformationView> {
                 const Padding(
                   padding: EdgeInsets.symmetric(horizontal: s16),
                   child: ContainerWithLabel(
-                    label: 'Điều khoản',
+                    label: 'Quy định',
                     child: Text(
-                      'Quy định khác:\n◦ Sử dụng xe đúng mục đích.\n◦ Không sử dụng xe thuê vào mục đích phi pháp, trái pháp luật.\n◦ Không sử dụng xe thuê để cầm cố, thế chấp.\n◦ Không hút thuốc, nhả kẹo cao su, xả rác trong xe.\n◦ Không chở hàng quốc cấm dễ cháy nổ.\n◦ Không chở hoa quả, thực phẩm nặng mùi trong xe.\n◦ Khi trả xe, nếu xe bẩn hoặc có mùi trong xe, khách hàng vui lòng vệ sinh xe sạch sẽ hoặc gửi phụ thu phí vệ sinh xe.\nTrân trọng cảm ơn, chúc quý khách hàng có những chuyến đi tuyệt vời !',
+                      '◦ Sử dụng xe đúng mục đích.\n◦ Không sử dụng xe thuê vào mục đích phi pháp, trái pháp luật.\n◦ Không sử dụng xe thuê để cầm cố, thế chấp.\n◦ Không hút thuốc, nhả kẹo cao su, xả rác trong xe.\n◦ Không chở hàng quốc cấm dễ cháy nổ.\n◦ Không chở hoa quả, thực phẩm nặng mùi trong xe.\n◦ Khi trả xe, nếu xe bẩn hoặc có mùi trong xe, khách hàng vui lòng vệ sinh xe sạch sẽ hoặc gửi phụ thu phí vệ sinh xe.\nTrân trọng cảm ơn, chúc quý khách hàng có những chuyến đi tuyệt vời !',
                       style: TextStyle(
                         fontSize: 12,
                         fontWeight: FontWeight.w500,
